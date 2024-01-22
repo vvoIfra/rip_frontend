7f39716b99ad6a92073f9a86a8bee8806a4eef67
Artem <vegard030@gmail.com>
2024-01-23 02:17:31 +0300

    Updated adapt

--- src/components/Ship_Card/Ship_Card.tsx
+++ src/components/Ship_Card/Ship_Card.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 // "data:image/jpeg;base64,"+
 const Ship_Card: FC<Props> = ({ ship_id, name, description, photo_data }) => (
-    <Card style={{ width: '20rem',height: '27rem',  marginRight: '2rem', marginTop: '2em'  }}>
+    <Card style={{marginRight: '2rem', marginTop: '2em'  }}>
 	    <Card.Body>
 	    	<Card.Title>{name}</Card.Title>
 	    	<Card.Img variant="top" src={`data:image/jpeg;base64,${photo_data}`} height={150} width={200} />
